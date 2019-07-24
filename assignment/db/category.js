6fd7df4531c1b8ee9c797d1bd2e1b239bcd5231e
priya R <priyaravi1297@gmail.com>
2019-07-25 01:59:47 +0530

    shopping cart

--- assignment/db/category.js
+++ assignment/db/category.js
@@ -22,28 +22,28 @@ const category = [
 	},
 	{
 		id : 4,
-		name : 'water bottle-180',
+		name : 'water bottle-300',
 		typ : 'q4',
 		price : 180,
 		quantity : 4
 	},
 	{
 		id : 5,
-		name : 'belt-230',
+		name : 'belt-350',
 		typ : 'q5',
 		price : 230,
 		quantity : 4
 	},
 	{
 		id : 6,
-		name : 'saree-2400',
+		name : 'saree-1400',
 		typ : 'q6',
 		price : 2400,
 		quantity : 6
 	},
 	{
 		id : 7,
-		name : 'toys-350',
+		name : 'toys-150',
 		typ : 'q7',
 		price : 350,
 		quantity : 4
